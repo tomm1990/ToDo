ae795dac4b0573fa37bdafbb3a4cae722c827861
TomGoldberg <tomg3210@gmail.com>
2017-03-12 10:44:32 +0200

    Final Version 1.0 - ToDo

--- tests/il/ac/shenkar/javaee/model/TestRun.java
+++ tests/il/ac/shenkar/javaee/model/TestRun.java
@@ -7,7 +7,6 @@ import org.junit.runner.notification.Failure;
 /**
  * Standalone application for run all tests realted to ToDo project
  * @author TomGoldberg, ItayNoama, OrBenDavid
- *
  */
 public class TestRun {
 	public static void main(String[] args) {
